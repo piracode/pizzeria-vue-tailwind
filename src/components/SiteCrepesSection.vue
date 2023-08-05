73eebf68b73794fc9ffaf1d450ee99be3e03a862
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-08-05 14:46:43 -0700

    center title crepes cards

--- src/components/SiteCrepesSection.vue
+++ src/components/SiteCrepesSection.vue
@@ -5,7 +5,8 @@
     <article
       class="p-4 m-4 bg-pizzaCardBg rounded-md drop-shadow-md text-black"
     >
-      <h3 class="pb-4">Savory Crepes</h3>
+      <h3 class="text-center">Crêpes Saladas</h3>
+      <p class="pb-4 text-center">(de harina blanca)</p>
       <ul>
         <li v-for="crepe in savoryCrepes" :key="crepe.id" class="pb-2">
           {{ crepe.ingredients }} - {{ crepe.price }}
@@ -16,7 +17,8 @@
     <article
       class="p-4 m-4 bg-pizzaCardBg rounded-md drop-shadow-md text-black"
     >
-      <h3 class="pb-4">Sweet Crepes</h3>
+      <h3 class="text-center">Crêpes dulces</h3>
+      <p class="pb-4 text-center">(de harina blanca)</p>
       <ul>
         <li v-for="crepe in sweetCrepes" :key="crepe.id" class="pb-2">
           {{ crepe.ingredients }} - {{ crepe.price }}
@@ -27,7 +29,7 @@
     <article
       class="p-4 m-4 bg-pizzaCardBg rounded-md drop-shadow-md text-black"
     >
-      <h3 class="pb-4">Gluten-Free Crepes</h3>
+      <h3 class="pb-4 text-center">Gluten-Free Crepes</h3>
       <ul>
         <li class="pb-2">
           A eligir entre las crêpes dulces o saladas (pueden contener trazas de
