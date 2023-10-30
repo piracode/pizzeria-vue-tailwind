4898d6c5ea50887cc781513dcaf8ee628d19ab84
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-10-29 20:16:54 -0700

    Fix config file with right path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
+//For developement
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production' ? '/pizzeria-arena' : '/',
+//   devServer: {
+//     historyApiFallback: true,
+//   },
+// })
+
+//For production
+
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/pizzeria-arena' : '/',
+  publicPath: '/',
   devServer: {
     historyApiFallback: true,
   },
